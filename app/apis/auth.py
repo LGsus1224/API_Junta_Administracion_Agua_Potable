ffd6f6a128d9ac20bae5ba4192b15c9795ab02f6
ElYisus1224 <gonzalezjesusetluna1@gmail.com>
2023-09-22 12:53:41 -0500

    Authentication api

---
+++ app/apis/auth.py
@@ -0,0 +1,71 @@
+from flask_restx import Namespace,Resource,fields,abort
+from flask_login import login_user,login_required,logout_user
+from app.models import Usuarios
+
+
+api = Namespace('Acceso y Autenticación', description='Endpoints para el acceso al sistema, autneticación y servicios relacionados')
+
+
+# Modelo de respuesta correcta
+success_message = api.model('Success Response',{
+    'success':fields.String(readonly=True, description='Success message')
+})
+
+
+# Modelo de respuesta erronea
+error_message = api.model('Error Response',{
+    'error':fields.String(readonly=True, description='Error message')
+})
+
+
+@api.route('/sign_in')
+class SignIn(Resource):
+    signIn_model = api.model('Inicio de sesión',{
+        'username':fields.String(required=True, description='Nombre del usuario'),
+        'password':fields.String(required=True, description='Contraseña de la cuenta del usuario')
+    })
+
+    @api.expect(signIn_model)
+    @api.response(200, 'OK', success_message)
+    @api.response(400, 'Bad Request', error_message)
+    def post(self):
+        """
+        Inicio de sesión con credenciales del usuario
+
+        Inicio de sesión del usuario
+        """
+        try:
+            data = api.payload
+            username = data['username']
+            password = data['password']
+            user = Usuarios.query.filter_by(username=username).first()
+            if user is None: raise Exception("Usuario o contraseña incorrecta")
+            if not user.check_password(user.password,password):
+                raise Exception("Usuario o contraseña incorrecta")
+            login_user(user)
+            return {
+                'success':'Inicio de sesión exitoso'
+            },200
+        except Exception as e:
+            abort(400, error='No se pudo iniciar la sesión: '+str(e))
+
+
+@api.route('/logout')
+class Logout(Resource):
+    @api.response(200, 'OK', success_message)
+    @api.response(400, 'Bad Request', error_message)
+    @login_required
+    def post(self):
+        """
+        Cerrar la sesión del usuario
+
+        Cierra la sesión del usuario
+        """
+        try:
+            logout_user()
+            return {
+                'success':'Sesion de usuario terminada'
+            },200
+        except Exception:
+            abort(400, error='Hubo un error al intentar cerrar la sesión')
+
